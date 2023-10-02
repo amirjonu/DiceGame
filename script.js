7d1cb2f30661e192cd8209d7b7bddec196764f37
Amirjon Ulmasov <amirjonu@nycstudents.net>
2023-10-02 14:46:46 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById("button");
+
+button.addEventListener("click", roll);
+
+/*function roll() {
+    const firstdie= document.getElementById("First Dice");
+    const seconddie= document.getElementById("Second Dice");
+    const sum = document.getElementById("sum");
+    let first= Math.floor(Math.random() * (6) + 1);
+    let second= Math.floor(Math.random() * (6) + 1);
+    let total= first+second; 
+    firstdie.textContent = `First die: ${first}`;
+    seconddie.textContent= `Second die: ${second}`;
+    sum.textContent=`Total: ${total}`;
+  }
+  */
